94dc89f09b85177ba00fa881560e91deb2e0cb43
Chris Ferguson <cfergus7@gmail.com>
2020-06-18 14:08:22 -0500

    newReservation line 56-75

--- express.js
+++ express.js
@@ -2,8 +2,17 @@
 // ===========================================================
 var express = require("express");
 
+// Sets up the Express App
+// =============================================================
+
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 // Data Starwars array of object as example
 // ===========================================================
@@ -14,26 +23,21 @@ var PORT = 3000;
 //     forcePoints: 2000
 // };
 
-var reservation = {
+var reservation = [{
+    routeName: "chrisferguson",
     name: "Chris Ferguson",
     phone: 1234567890,
     email: "me@gmail.com",
     uniqueId: 1
-};
+}, ];
 
-var waitlist = {
-
-};
-
-
-
-// Create one more data entry for the character Obi Wan Kenobi.
-// Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
-//
-
-// YOUR CODE GOES HERE
-
-
+var waitlist = [{
+    routeName: "ryanferguson"
+    name: "Ryan Ferguson",
+    phone: 1234567800,
+    email: "u@gmail.com",
+    uniqueId: 2
+}, ];
 
 // Routes
 // ===========================================================
@@ -49,13 +53,26 @@ app.get("/waitlist", function (req, res) {
     res.json(waitlist);
 });
 
+// Create New Reservations - takes in JSON input
+app.post("/api/reservation", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
 
-// Create a new Express route that leads users to the new Obi Wan Kenobi Data
-// Follow the same format as the Yoda and Darth Maul routes
-//
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    newReservation.phone = parseInt(newReservation.phone);
+    newReservation.email = parseInt(newReservation.email);
+    newReservation.uniqueId = parseInt(newReservation.uniqueId);
 
-// YOUR CODE GOES HERE
 
+    console.log(newReservation);
+
+    reservation.push(newReservation);
+
+    res.json(newReservation);
+});
 
 
 // Listener
